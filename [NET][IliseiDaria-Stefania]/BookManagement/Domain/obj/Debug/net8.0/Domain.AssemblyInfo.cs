b132f385c606d009b16373faf1a971fe13277072
Ilisei Daria-Stefania <iliseidaria27@gmail.com>
2024-11-21 17:35:15 +0200

    version presented during laboratory

--- [NET][IliseiDaria-Stefania]/BookManagement/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ [NET][IliseiDaria-Stefania]/BookManagement/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a31cf13cb7c383bae8af2a3908f763508e7ce14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- [NET][IliseiDaria-Stefania]/BookManagement/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ [NET][IliseiDaria-Stefania]/BookManagement/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a31cf13cb7c383bae8af2a3908f763508e7ce14")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
